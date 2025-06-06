329690ff9d27397d03fc7294c8d4b7da3239aabb
x33rioz <rostrinski@gmail.com>
2025-06-06 16:10:54 +0300

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,10 @@
+﻿// This is a comment to my project
+// rakendus palub kasutajalt sisestada oma nimet
+// rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name:");//Output
+string userName;
+userName = Console.ReadLine();//Input
+Console.WriteLine("Hello"+","+userName+"!");//Output
+// string interpolation
+Console.WriteLine($"Hello,{userName} !");//Input
